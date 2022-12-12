ea908b29857224e39b4bd773a9159af9631387f0
caiosqueiroz <119979275+caiosqueiroz@users.noreply.github.com>
2022-12-12 18:36:29 -0300

    first commit

---
+++ appcaio.py
@@ -0,0 +1,115 @@
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import numpy as np
+ 
+import plotly.express as px
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
+
+from dash_bootstrap_templates import load_figure_template
+
+load_figure_template("minty")
+
+
+app = dash.Dash(external_stylesheets=[dbc.themes.MINTY])
+server = app.server
+
+
+# =========  dados  =========== #
+
+df_data = pd.read_csv("supermarket_sales.csv")
+df_data["Date"] = pd.to_datetime(df_data["Date"])
+
+df_data
+
+# =========  Layout  =========== #
+
+
+
+
+
+app.layout = html.Div(children= [
+               
+               dbc.Row([
+                   dbc.Col([
+                       dbc.Card([
+                           
+                              html.H5('Cidades:'),
+                              #checklist pede 2 parametros : valores e quais estão marcados
+                              dcc.Checklist(df_data['City'].unique(),
+                              df_data['City'].unique(), id = 'check_city' ),
+                
+                            html.H5('Variável de análise:'),
+                            dcc.RadioItems(['gross income', 'Rating'], 'gross income', id ='main_variable'),]),] sm = 2),
+                           
+                            ]),
+                       
+                   
+                   dbc.Col([
+                       dcc.Graph(id='city_fig'),
+                       dcc.Graph(id='pay_fig'),
+                       dcc.Graph(id='income_per_product_fig'),
+                       ], sm = 10)
+                   
+               ])
+               
+               
+               
+                
+                
+                
+                
+                
+])
+
+# =========  Callbacks  =========== #
+
+#1º outputs que são os gráficos
+
+@app.callback([
+               Output('city_fig', 'figure'),
+               Output('pay_fig', 'figure'),
+               Output('income_per_product_fig', 'figure'),
+            ],
+                  [
+                   #quem irá controlar o gráfico 1 será o 'value' do checklist das cidades
+                   
+                   Input('check_city', 'value'), #        
+                   Input('main_variable', 'value'),
+               ])          
+               
+def render_graphs(cities, main_variable):
+    # cities = ['Yangon','Mandalay']
+    # main_variable = 'gross income'
+    operation = np.sum if main_variable == 'gross income' else np.mean
+    
+    #tratando dados 
+    df_filtred = df_data[df_data['City'].isin(cities)] #filtrando os dados que estão com as cidades selecionadas
+    
+    #calculando graficos para cada output
+    df_city = df_filtred.groupby('City')[main_variable].apply(operation).to_frame().reset_index()
+    df_payment = df_filtred.groupby('Payment')[main_variable].apply(operation).to_frame().reset_index()
+    df_product_income = df_filtred.groupby(['Product line','City'])[main_variable].apply(operation).to_frame().reset_index()
+
+    #configurando os gráficos
+    fig_city = px.bar(df_city, x='City', y=main_variable)
+    fig_payment = px.bar(df_payment, y='Payment', x=main_variable, orientation = 'h')
+    fig_product_income = px.bar(df_product_income, x= main_variable, y='Product line', color='City', orientation = 'h', barmode='group')
+    
+    fig_city.update_layout(margin=dict (l=0, r=0, t=20, b=20), height=200)
+    fig_payment.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200)
+    fig_payment.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=500)
+    
+    
+    
+    return fig_city, fig_payment, fig_product_income
+
+
+
+# =========  Run server  =========== #
+if __name__ == "__main__":
+    app.run_server(port=8050, debug=False)
+    
